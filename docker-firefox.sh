a21b4bf2f7b0e445ec1616db7159a5a141439ff5
Dennis Schridde <devurandom@gmx.net>
2018-01-23 18:07:39 +0100

    Mount CUPS Unix socket into container, instead of making it connect via TCP
    The default configuration for CUPS is to make it listen on localhost -- hence
     we cannot connect from a container that runs on a different IP.

--- docker-firefox.sh
+++ docker-firefox.sh
@@ -18,7 +18,7 @@ container_exists() {
 
 docker_address="$(ip address show dev docker0 | egrep '\<inet\>' | awk '{print$2}' | cut -d/ -f1)"
 
-docker run --detach --publish 30000:14500 --user "$(id -u):$(id -g)" --volume "${FIREFOX_STORAGE}":/home:rw --volume /var/run/nscd/socket:/var/run/nscd/socket:rw --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro --env XPRA_EXTRA_ARGS="--tcp-auth= --tcp-encryption=" --env CUPS_SERVER="${docker_address}" --env SOCKS_SERVER="${docker_address}:5080" --env SOCKS_VERSION=5 "${dri_devices[@]}" "${CONTAINER_IMAGE}" "$@"
+docker run --detach --publish 30000:14500 --user "$(id -u):$(id -g)" --volume "${FIREFOX_STORAGE}":/home:rw --volume /var/run/nscd/socket:/var/run/nscd/socket:rw --volume /var/run/cups/cups.sock:/var/run/cups/cups.sock:rw --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro --env XPRA_EXTRA_ARGS="--tcp-auth= --tcp-encryption=" --env SOCKS_SERVER="${docker_address}:5080" --env SOCKS_VERSION=5 "${dri_devices[@]}" "${CONTAINER_IMAGE}" "$@"
 
 while ! curl --silent http://localhost:30000/Status | grep --quiet 'disconnect: invalid packet header' ; do
 	sleep 1
