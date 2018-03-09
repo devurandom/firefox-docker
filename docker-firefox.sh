8d1b49f83b7d5b0331bff32cb6b4aa7f73ed40bb
Dennis Schridde <devurandom@gmx.net>
2018-03-09 19:31:17 +0100

    Ensure the container image is always up-to-date

--- docker-firefox.sh
+++ docker-firefox.sh
@@ -18,6 +18,8 @@ container_exists() {
 
 docker_address="$(ip address show dev docker0 | egrep '\<inet\>' | awk '{print$2}' | cut -d/ -f1)"
 
+docker pull "${CONTAINER_IMAGE}"
+
 docker run --detach --publish 30000:14500 --user "$(id -u):$(id -g)" --volume "${FIREFOX_STORAGE}":/home:rw --volume /var/run/nscd/socket:/var/run/nscd/socket:rw --volume /var/run/cups/cups.sock:/var/run/cups/cups.sock:rw --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro --env XPRA_EXTRA_ARGS="--tcp-auth= --tcp-encryption=" --env SOCKS_SERVER="${docker_address}:5080" --env SOCKS_VERSION=5 "${dri_devices[@]}" "${CONTAINER_IMAGE}" "$@"
 
 while ! curl --silent http://localhost:30000/Status | grep --quiet 'Message: this port does not support HTTP requests.' ; do
